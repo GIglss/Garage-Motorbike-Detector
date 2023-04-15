4175551496f883afb789d76454f7de524653ffeb
GIglss <gon.iglesias.g@gmail.com>
2023-04-15 16:20:18 +0200

    checkpoint

--- docker/detector/test_raspberry_pins.py
+++ docker/detector/test_raspberry_pins.py
@@ -6,11 +6,11 @@ GPIO.setmode(GPIO.BOARD) # definimos que la numeracion será en base  GPIO BOARD
 # esta numeracion pone al pin superior izq como 1 y al pin superios derecho como 2, así sucesivamente
 GPIO.cleanup()
 
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
 # loop through 50 times, on/off for 1 second
-for i in range(5):
-	GPIO.output(7,True)
-	time.sleep(1)
-	GPIO.output(7,False)
-	time.sleep(1)
+for i in range(30):
+	GPIO.output(5,GPIO.HIGH)
+	time.sleep(2)
+	GPIO.output(5,False)
+	time.sleep(2)
 GPIO.cleanup()
